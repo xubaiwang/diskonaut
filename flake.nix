c100c411a437121fcefdb3cd8af39a64154a1e05
王虚白 <18016038327@189.cn>
2025-05-07 22:12:42 +0800

    chore(nix): add nix flake
    chore: update cargo dependency version

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Rust dev shell template";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            packages = [
+              clippy
+              rustfmt
+              rust-analyzer
+            ];
+            nativeBuildInputs = [
+              cargo
+              pkg-config
+              rustc
+            ];
+          };
+
+        packages.diskonaut = pkgs.rustPlatform.buildRustPackage {
+          pname = "diskonaut";
+          version = "0.11.0";
+
+          src = ./.;
+
+          cargoHash = "sha256-F5/3ekwua5GaCW8vRU0HU708ThXRWleeah0Nrzlbnf8=";
+        };
+
+        packages.default = packages.diskonaut;
+      }
+    );
+}
